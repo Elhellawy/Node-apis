02a37aef4c35acfedb1604801d8dd46f5a8d2319
elhellawy <abdo.hellawy@gmail.com>
2019-01-02 18:37:18 +0200

    reomve middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
+//const fs = require('fs');
 
 var app = express();
 
@@ -9,17 +9,18 @@ const port=process.env.PORT || 3000
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
   fs.appendFile('server.log', log + '\n');
   next();
-});
+});*/
 
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
+
 // });
 
 app.use(express.static(__dirname + '/public'));
